7c6397efb6a9f56a1a605f6361edcf87e3a3baeb
hsuehic <hsuehic@163.com>
2016-07-12 01:00:58 +0800

    add chart pages

---
+++ src/components/page/Bar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <header-component/>
+        <div>this is template body</div>
+        <other-component/>
+    </div>
+</template>
+<style>
+    body{
+        background-color:#ff0000;
+    }
+</style>
+<script>
+    export default{
+        data () {
+            return {
+                msg: 'hello vue'
+            }
+        },
+        components:{
+        }
+    }
+</script>
