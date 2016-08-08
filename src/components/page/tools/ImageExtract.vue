9ae40f3bdbca9cdc4a29e5bd9f9e23ef8a351066
薛晓伟 <xuexiaowei@megvii.com>
2016-08-08 11:40:32 +0800

    refractor: 删除多余的文件

--- src/components/page/tools/ImageExtract.vue
+++ src/components/page/tools/ImageExtract.vue
@@ -2,7 +2,7 @@
     <image-extract :path = "path" :rect = "[200,200,600,400]"></image-extract>
 </template>
 <script>
-    import ImageExtract from '../../ui/ImageExtract.vue'
+    import ImageExtract from '../../ui/image/ImageExtract.vue'
     export default{
         data () {
             return {
---
+++ src/components/ui/popup/Popup.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+    </div>
+</template>
+<style>
+</style>
+<script>
+    export default{
+        replace: true,
+        data () {
+            return {
+                msg: 'hello vue'
+            }
+        },
+        props: {
+            size: {
+                type: Number
+            }
+        },
+        computed: {
+            encodeMsg () {
+                return escape(msg)
+            }
+        },
+        methods: {
+            showMsg () {
+                console.log(msg)
+            }
+        },
+        init () {
+        },
+        created () {
+        },
+        beforeCompile () {
+        },
+        compiled () {
+        },
+        ready () {
+        },
+        attached () {
+        },
+        detached () {
+        },
+        beforeDestroy () {
+        },
+        destroyed () {
+        },
+        watch: {
+            msg: function (val, oldVal) {
+                console.log('new: %s, old: %s', val, oldVal)
+            }
+        },
+        components: {
+        }
+    }
+</script>
