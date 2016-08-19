64feacdc0ea93ac44e8b866d096b46c102694d86
薛晓伟 <xuexiaowei@megvii.com>
2016-08-19 09:39:26 +0800

    feat: 添加引用

--- src/components/ui/image/ImageExtract.vue
+++ src/components/ui/image/ImageExtract.vue
@@ -50,18 +50,6 @@
                 canvas.width = width
                 canvas.height = height
                 var ctx = canvas.getContext('2d')
-                // context.drawImage(img,x,y);
-                // context.drawImage(img,x,y,width,height);
-                // context.drawImage(img,sx,sy,swidth,sheight,x,y,width,height);
-                // img	规定要使用的图像、画布或视频。
-                // sx	可选。开始剪切的 x 坐标位置。
-                // sy	可选。开始剪切的 y 坐标位置。
-                // swidth	可选。被剪切图像的宽度。
-                // sheight	可选。被剪切图像的高度。
-                // x	在画布上放置图像的 x 坐标位置。
-                // y	在画布上放置图像的 y 坐标位置。
-                // width	可选。要使用的图像的宽度。（伸展或缩小图像）
-                // height	可选。要使用的图像的高度。（伸展或缩小图像）
                 ctx.drawImage(source, self.rect[0], self.rect[1], self.rect[2], self.rect[3], 0, 0, width, height)
                 ctx.save()
                 target.setAttribute('src', canvas.toDataURL('image/png'))
